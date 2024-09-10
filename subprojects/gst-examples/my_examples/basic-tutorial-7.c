017c5bab28b55c99fdf8c8f60bc5d4f0788eefda
william <460706836@qq.com>
2024-09-10 21:12:50 +0800

    basic-tutorial-7.c can visual wave

--- subprojects/gst-examples/my_examples/basic-tutorial-7.c
+++ subprojects/gst-examples/my_examples/basic-tutorial-7.c
@@ -7,6 +7,7 @@
 int tutorial_main(int argc, char *argv[]) {
   GstElement *pipeline;
   GstElement *audio_source;
+  GstElement *queue;
   GstElement *visual;
   GstElement *video_convert;
   GstElement *video_sink;
@@ -16,6 +17,7 @@ int tutorial_main(int argc, char *argv[]) {
 
   // create elements
   audio_source = gst_element_factory_make("audiotestsrc", "audio_source");
+  queue = gst_element_factory_make("queue", "queue");
   visual = gst_element_factory_make("wavescope", "visual");
   video_convert = gst_element_factory_make("videoconvert", "video_convert");
   video_sink = gst_element_factory_make("autovideosink", "video_sink");
@@ -23,7 +25,7 @@ int tutorial_main(int argc, char *argv[]) {
   // create pipeline
   pipeline = gst_pipeline_new("test-pipeline");
 
-  if (!pipeline || !audio_source || !visual || !video_convert || !video_sink) {
+  if (!pipeline || !audio_source || !queue ||!visual || !video_convert || !video_sink) {
     g_printerr("Not all elements could be created.\n");
     return -1;
   }
@@ -33,8 +35,8 @@ int tutorial_main(int argc, char *argv[]) {
   g_object_set(visual, "shader", 0, "style", 1, NULL);
 
   // link elements
-  gst_bin_add_many(GST_BIN(pipeline), audio_source, visual, video_convert, video_sink, NULL);
-  if(gst_element_link_many(audio_source, visual, video_convert, video_sink, NULL) != TRUE) {
+  gst_bin_add_many(GST_BIN(pipeline), audio_source, queue, visual, video_convert, video_sink, NULL);
+  if(gst_element_link_many(audio_source, queue, visual, video_convert, video_sink, NULL) != TRUE) {
     g_printerr("Elements could not be linked.\n");
     gst_object_unref(pipeline);
     return -1;
