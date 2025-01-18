7206fad4b4664db738b2bc0434b65ee183402f20
william <huangyunfeng@xiaohongshu.com>
2025-01-18 21:11:07 +0800

    update gstmyfilter_example.c, can playback audio file

--- subprojects/gst-examples/my_examples/my_plugin/gstmyfilter_example.c
+++ subprojects/gst-examples/my_examples/my_plugin/gstmyfilter_example.c
@@ -1,75 +1,182 @@
 //
 // Created by user on 1/18/25.
 //
-
+//
+// Created by user on 6/14/24.
+//
 #include <gst/gst.h>
 
 #ifdef __APPLE__
 #include <TargetConditionals.h>
 #endif
 
-int
-tutorial_main (int argc, char *argv[])
-{
+typedef struct _CustomData {
   GstElement *pipeline;
-  GstBus *bus;
-  GstMessage *msg;
+  GstElement *source;
+  GstElement *convert;
+  GstElement *resample;
+  GstElement *my_filter;
+  GstElement *sink;
+} CustomData;
 
-  /* Initialize GStreamer */
-  gst_init (&argc, &argv);
+static void pad_added_handler(GstElement *src, GstPad *pad, CustomData *data);
 
-  /* Build the pipeline */
-  pipeline = gst_pipeline_new("pipeline");
-  GstElement* audio_test_src = gst_element_factory_make("audiotestsrc", "audio_test_src");
-  GstElement* my_filter = gst_element_factory_make("my_filter", "my_filter");
-  GstElement* audio_sink = gst_element_factory_make("autoaudiosink", "audio_sink");
+int tutorial_main(int argc, char *argv[]) {
+  CustomData data;
 
-  if(!pipeline || !audio_test_src || !my_filter || !audio_sink) {
-    g_printerr ("Not all elements could be created.\n");
+  // init
+  gst_init(&argc, &argv);
+
+  // create the elements
+  data.source = gst_element_factory_make("uridecodebin", "source");
+  data.convert = gst_element_factory_make("audioconvert", "convert");
+  data.resample = gst_element_factory_make("audioresample", "resample");
+  data.my_filter = gst_element_factory_make("my_filter", "my_filter");
+  data.sink = gst_element_factory_make("autoaudiosink", "sink");
+
+  // create pipeline
+  data.pipeline = gst_pipeline_new("test-pipeline");
+
+  if (!data.pipeline || !data.source || !data.convert || !data.resample ||
+      !data.my_filter ||!data.sink) {
+    g_printerr("Not all elements could be created.\n");
     return -1;
   }
 
-  gst_bin_add_many(GST_BIN(pipeline), audio_test_src, my_filter, audio_sink, NULL);
-  gboolean ok = gst_element_link_many(audio_test_src, my_filter, audio_sink, NULL);
-  if(!ok) {
-    g_printerr ("Elements could not be linked.\n");
-    gst_object_unref (pipeline);
+  // build the pipeline. Note that we are NOT linking the source at this point.
+  gst_bin_add_many(GST_BIN(data.pipeline), data.source, data.convert,
+                   data.resample, data.my_filter, data.sink, NULL);
+  if (!gst_element_link_many(data.convert, data.resample, data.my_filter, data.sink, NULL)) {
+    g_printerr("Elements could not be linked.\n");
+    gst_object_unref(data.pipeline);
     return -1;
   }
 
-  g_object_set(audio_test_src, "wave", 12, NULL);
-  g_object_set(my_filter, "delay", 1000.0f, NULL);
-  g_object_set(my_filter, "feedback", 0.2f, NULL);
+  // set uri to play
+  g_object_set(
+      data.source, "uri",
+      "file:///Users/user/Documents/work/测试视频/video_1280x720_30fps_180sec.mp4",
+      NULL);
 
-  /* Start playing */
-  gst_element_set_state (pipeline, GST_STATE_PLAYING);
+  // set my filter parameters
+  g_object_set(data.my_filter, "delay", 1000.0f, NULL);
+  g_object_set(data.my_filter, "feedback", 0.5f, NULL);
 
-  /* Wait until error or EOS */
-  bus = gst_element_get_bus (pipeline);
-  msg =
-      gst_bus_timed_pop_filtered (bus, GST_CLOCK_TIME_NONE,
-      GST_MESSAGE_ERROR | GST_MESSAGE_EOS);
+  // connect to the pad-added signal
+  g_signal_connect(data.source, "pad-added", G_CALLBACK(pad_added_handler),
+                   &data);
 
-  /* See next tutorial for proper error message handling/parsing */
-  if (GST_MESSAGE_TYPE (msg) == GST_MESSAGE_ERROR) {
-    g_printerr ("An error occurred! Re-run with the GST_DEBUG=*:WARN "
-        "environment variable set for more details.\n");
+  // start playing
+  GstStateChangeReturn ret =
+      gst_element_set_state(data.pipeline, GST_STATE_PLAYING);
+  if (ret == GST_STATE_CHANGE_FAILURE) {
+    g_printerr("Unable to set the pipeline to the playing state.\n");
+    gst_object_unref(data.pipeline);
+    return -1;
   }
 
-  /* Free resources */
-  gst_message_unref (msg);
-  gst_object_unref (bus);
-  gst_element_set_state (pipeline, GST_STATE_NULL);
-  gst_object_unref (pipeline);
+  GstBus *bus = gst_element_get_bus(data.pipeline);
+  GstMessage *msg = NULL;
+  gboolean terminate = FALSE;
+  do {
+    msg = gst_bus_timed_pop_filtered(bus, GST_CLOCK_TIME_NONE,
+                                     GST_MESSAGE_STATE_CHANGED |
+                                         GST_MESSAGE_ERROR | GST_MESSAGE_EOS);
+    if (msg != NULL) {
+      GError *err;
+      gchar *debug_info;
+
+      switch (GST_MESSAGE_TYPE(msg)) {
+      case GST_MESSAGE_ERROR:
+        gst_message_parse_error(msg, &err, &debug_info);
+        g_printerr("Error received from element %s: %s\n",
+                   GST_OBJECT_NAME(msg->src), err->message);
+        g_printerr("Debugging information: %s\n",
+                   debug_info ? debug_info : "none");
+        g_clear_error(&err);
+        g_free(debug_info);
+        terminate = TRUE;
+        break;
+      case GST_MESSAGE_EOS:
+        g_print("End-Of-Stream reached.\n");
+        terminate = TRUE;
+        break;
+      case GST_MESSAGE_STATE_CHANGED:
+        if (GST_MESSAGE_SRC(msg) == GST_OBJECT(data.pipeline)) {
+          GstState old_state;
+          GstState new_state;
+          GstState pending_state;
+          gst_message_parse_state_changed(msg, &old_state, &new_state,
+                                          &pending_state);
+          g_print("Pipeline state changed from %s to %s, %s:\n",
+                  gst_element_state_get_name(old_state),
+                  gst_element_state_get_name(new_state),
+                  gst_element_state_get_name(pending_state));
+        }
+        break;
+      default:
+        /* We should not reach here */
+        g_printerr("Unexpected message received.\n");
+        break;
+      }
+
+      gst_message_unref(msg);
+    }
+  } while (!terminate);
+
+  // free resources
+  gst_object_unref(bus);
+  gst_element_set_state(data.pipeline, GST_STATE_NULL);
+  gst_object_unref(data.pipeline);
   return 0;
 }
 
-int
-main (int argc, char *argv[])
-{
+static void pad_added_handler(GstElement *src, GstPad *new_pad, CustomData *data) {
+  GstPad *sink_pad = gst_element_get_static_pad (data->convert, "sink");
+  GstPadLinkReturn ret;
+  GstCaps *new_pad_caps = NULL;
+  GstStructure *new_pad_struct = NULL;
+  const gchar *new_pad_type = NULL;
+
+  g_print ("Received new pad '%s' from '%s':\n", GST_PAD_NAME (new_pad), GST_ELEMENT_NAME (src));
+
+  // if converter is already linked, do nothing here
+  if(gst_pad_is_linked(sink_pad)){
+    g_print ("We are already linked. Ignoring.\n");
+    goto exit;
+  }
+
+  new_pad_caps = gst_pad_get_current_caps(new_pad);
+  new_pad_struct = gst_caps_get_structure(new_pad_caps, 0);
+  new_pad_type = gst_structure_get_name(new_pad_struct);
+
+  // if not audio pad type, skip
+  if (!g_str_has_prefix (new_pad_type, "audio/x-raw")) {
+    g_print ("It has type '%s' which is not raw audio. Ignoring.\n", new_pad_type);
+    goto exit;
+  }
+
+  // try to link
+  ret = gst_pad_link(new_pad, sink_pad);
+  if (GST_PAD_LINK_FAILED (ret)) {
+    g_print ("Type is '%s' but link failed.\n", new_pad_type);
+  } else {
+    g_print ("Link succeeded (type '%s').\n", new_pad_type);
+  }
+
+exit:
+  /* Unreference the new pad's caps, if we got them */
+  if (new_pad_caps != NULL)
+    gst_caps_unref (new_pad_caps);
+
+  /* Unreference the sink pad */
+  gst_object_unref (sink_pad);
+}
+
+int main(int argc, char *argv[]) {
 #if defined(__APPLE__) && TARGET_OS_MAC && !TARGET_OS_IPHONE
-  return gst_macos_main ((GstMainFunc) tutorial_main, argc, argv, NULL);
+  return gst_macos_main((GstMainFunc)tutorial_main, argc, argv, NULL);
 #else
-  return tutorial_main (argc, argv);
+  return tutorial_main(argc, argv);
 #endif
 }
